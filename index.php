c1697597cfac21cff671743c7073a8056138c846
xTOMATOx <xTOMATOx@users.noreply.github.com>
2020-11-08 23:34:37 +0800

    Update Functionality
    Added reusable methods for select, insert, and update values on database

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 require_once 'core/init.php';
 
-$users = DB::getInstance->query('SELECT username FROM users');
-if($users->count())
-{
-    foreach($users as $user)
-    {
-        echo $user->username
-    }
-}
-?>
+$user = DB::getInstance()->update('users','2',array(
+    'password' => 'HelloAnotherWorld',
+    'name' => 'Christopher Davidson'
+));
